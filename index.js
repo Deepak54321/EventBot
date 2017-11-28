6a7c95233e34bb05eaf5f97a355d788915f6ef6a
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-11-28 09:50:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,33 +36,28 @@ app.post('/webhook', function (req, res) {
 
   // parameters are stored in req.body.result.parameters
   //var userName = req.body.result.parameters['given-name']
-    //var context=req.body.result.contexts[0];
-  if(req.body.result.action=='demo')
+    var context=req.body.result.contexts[0];
+  if(context.action=='demo')
   {
-    var request = require('request');
-          /*  request({
-                url:'http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/statewiseprice/getprice?product_profile_id=salutorxspcol&state_id=240'
-            },function (error,response,body) {
-                if (!error && response.statusCode == 200) {
-                    var result = JSON.parse(body);
-                    var responseCode=result.responseData;
-                    var productPrice=responseCode.product_price;
-                    var price=productPrice[0].price +'Rs';*/
-    var price='sdjkflsl'
-                  res.status(200).json({
+    var webhookReply = 'Hello Welcome from the webhook Demo.'
+  // the most basic response
+  res.status(200).json({
     source: 'webhook',
-    speech: price,
-    displayText: price
+    speech: webhookReply,
+    displayText: webhookReply
   })
-//})
-              // }
-               
-  //          });  
   }
-if(req.body.result.action=='demo1')
+if(context.action=='demo1')
   {
     
- }
+    var webReply = 'Hello Welcome from the webhook Demo1.'
+  // the most basic response
+  res.status(200).json({
+    source: 'webhook',
+    speech: webReply,
+    displayText: webReply
+  })
+  }
   
 })
 
