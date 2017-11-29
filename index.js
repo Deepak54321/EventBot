3605f3687f17b7fada18c666c9124845ee772411
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-11-29 17:35:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ if(req.body.result.action=='dealer')
                                             var dealer_add = dealers[0].dealer_address;
                                             var dealer_Mob = dealers[0].sales_manager_mobile;
                                             var text1 = dealer_name + dealer_add + dealer_Mob;
-                                            
+                                            text1 = "Helloa";
                                             console.log("Dealer information %s",text1);
                                             //if(text1!='') {
                                              res.status(200).json({
