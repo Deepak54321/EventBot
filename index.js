4b312d943a330c3ebe4d18c953d34328aca75ff3
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-11-28 22:19:27 -0800

    IC6

--- index.js
+++ index.js
@@ -115,15 +115,85 @@ if(req.body.result.action=='demo1')
                     console.log("Country %s",Country);
                     //console.log(city);
                     var view = State + City + Country + 'Hi now you can get your dealers' + lat + lng;
-					 res.status(200).json({
-           source: 'webhook',
-           speech: view,
-           displayText: view
-            })
+			
                     //2
 				}
 
             });
+			//2
+			request({
+                        url: 'http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/network/state'
+                    }, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var res = JSON.parse(body);
+                            var responseData = res.responseData;
+                            var states = responseData.states;
+
+                            for (var i = 0; i < states.length; i++) {
+                                if (states[i].state_name === State) {
+                                    StateId = states[i].profile_id;
+                                    State_Name = states[i].state_name;
+
+                                }
+
+                            }
+                            console.log("State Id %s",StateId);
+                            if(StateId=='') {
+                                //sendQuickReply(sender,"No dealers Found in your area Please restart your conversation", reply2);
+                            }
+
+						});
+						//3
+						   request({
+                                url: 'http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/network/city?profile_id=' + StateId
+                            }, function (error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    var result = JSON.parse(body);
+                                    var responsData = result.responseData;
+                                    var citites = responsData.cities;
+                                    for (var i = 0; i < citites.length; i++) {
+
+                                        if (citites[i].city_name == City) {
+                                            CityId = citites[i].city_profile_id;
+                                        }
+                                    }
+                                    console.log("City Id %s",CityId);
+                                    if(CityId=='') {
+                                        //sendQuickReply(sender,"No dealers Found in your area Please restart your conversation", reply3);
+                                    }
+								});
+								//4
+								
+                                    request({
+                                        url: 'http://www.yamaha-motor-india.com/iym-web-api//51DCDFC2A2BC9/network/search?type=sales&profile_id=' + StateId + '&city_profile_id=' + CityId
+                                    }, function (error, response, body) {
+                                        if (!error && response.statusCode == 200) {
+                                            var result = JSON.parse(body);
+                                            var resData = result.responseData;
+                                            var dealers = resData.dealers;
+                                            dealerId=dealers[0].dealer_name;
+                                            var dealer_name = dealers[0].dealer_name;
+                                            var dealer_add = dealers[0].dealer_address;
+                                            var dealer_Mob = dealers[0].sales_manager_mobile;
+                                            var text1 = dealer_name + dealer_add + dealer_Mob;
+                                            console.log("Dealer information %s",text1);
+                                            if(text1!='') {
+                                                		 res.status(200).json({
+           source: 'webhook',
+           speech: text1,
+           displayText: text1
+            })
+                                            }
+                                            else
+                                            {
+                                                
+                                            }
+                                         
+                                        }
+                                        else {
+                                            console(log.error());
+                                        }
+                                    });
 
   }
   
