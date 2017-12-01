0b0f6ecbf2523b404d9f31b51cd6975c2efdd61b
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-12-01 03:14:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/webhook', function (req, res) {
    function printdealer(res,message)
    {
 	    res.status(200).json({
-           					source: 'webhook',
+           					/*source: 'webhook',
           					speech: message,
            					displayText: message,
 		    				'messages': 
@@ -34,7 +34,8 @@ app.post('/webhook', function (req, res) {
                   				{'title': 'Please provide your feedback',
                 				'replies': ['Feedback'],
                 				'type': 2}],
-            					})
+            					})*/
+		
    }
   if(req.body.result.action=='Priceapi')
   {
@@ -71,7 +72,7 @@ app.post('/webhook', function (req, res) {
 	if(req.body.result.action=='hotel')
   {
    res.status(200).json({
-           					source: 'webhook',
+           				/*	source: 'webhook',
           					speech: 'message',
            					displayText: 'message',
 		    				"followupEvent":{
@@ -82,7 +83,21 @@ app.post('/webhook', function (req, res) {
 								"check_out":"",
 								"adults":"3"
 							}
-						}
+						}*/
+	       "speech": "The value was invalid so I changed it to its default. What is your value for MyField2?",
+            "displayText": "The value was invalid so I changed it to its default. What is your value for MyField2?",
+            "contextOut": [
+                {
+                    //"name": "dc324995-cd5d-48b2-a74d-d2fca6fa0cc3_id_dialog_context",
+			"name":"HotelBooking",
+                    "parameters": {
+                         "check_in":"",
+								"check_out":"",
+								"adults":"3"
+                    },
+                    "lifespan": 2
+                }
+            ]
             					})
   }
 if(req.body.result.action=='Dealerapi')
