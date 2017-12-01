14368b80787e6329052706a67d2374f776138eae
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-12-01 03:52:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/webhook', function (req, res) {
               [{
                    'type':0,
                    'speech':message
-               }
+               }]
             })
 	  }
 	  else
