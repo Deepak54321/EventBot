fc671e3bf7f21b71e46f6ee2233d3041aa5d6c07
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-12-06 03:31:19 -0800

    dsfsd

--- index.js
+++ index.js
@@ -100,6 +100,47 @@ app.post('/webhook', function (req, res) {
   
   }
 	
+	 	if(req.body.result.action=='pin-val')
+  {
+	   var result=req.body.result;
+			var context=result.contexts[0];
+			var Pin=context.parameters.pincode;
+			var regex = /^([1-9])([0-9]){5}$/;
+			 //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+	  console.log("%s",Pin);
+	  if(regex.test(Pin))
+	  {
+		  var message='please share your chasis Number';
+		    res.status(200).json({
+           source: 'webhook',
+           //speech: message,
+           //displayText: message,
+		    'messages': 
+              [{
+                   'type':0,
+                   'speech':message
+               }]
+            })
+	  }
+	  else
+	  {
+		   res.status(200).json({
+           					source: 'webhook',
+          					speech: 'Invalid email Please enter again',
+           					displayText: 'Invalid email Please enter again',
+		    				"followupEvent":{
+						"name":"re_pin",
+							"data":
+							{
+								"pincode":""
+							}
+						}
+	    
+            					})
+	  }
+  
+  }
+	
 	if(req.body.result.action=='hotel')
   {
 	  var adult=7;
