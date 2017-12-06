f7974b95bd4065d05e909ba1d876f21bbe8f790e
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-12-06 00:41:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,48 @@ app.post('/webhook', function (req, res) {
   }
 	
 	if(req.body.result.action=='phone')
-  { res.status(200).json({
+  {
+	   var result=req.body.result;
+			var context=result.contexts[0];
+			var PhoneNumber=context.parameters.phonenumber;
+	  console.log("%s",PhoneNumber);
+	  if(PhoneNumber.length>10)
+	  {
+		  var message='please share your email';
+		    res.status(200).json({
+           source: 'webhook',
+           speech: message,
+           displayText: message,
+		    'messages': 
+              [{
+                   'type':0,
+                   'speech':message
+               }]
+            })
+	  }
+	  else
+	  {
+		   res.status(200).json({
+           					source: 'webhook',
+          					speech: 'Invalid phone Number Please enter again',
+           					displayText: 'Invalid phone Number Please enter again',
+		    				"followupEvent":{
+						"name":"re_ask",
+							"data":
+							{
+								"phonenumber":""
+							}
+						}
+	    
+            					})
+	  }
+  
+  }
+	
+	if(req.body.result.action=='hotel')
+  {
+	  var adult=7;
+   res.status(200).json({
            				source: 'webhook',
           					speech: 'message',
            					displayText: 'message',
@@ -68,40 +109,21 @@ app.post('/webhook', function (req, res) {
 								"adults":adult
 							}
 						}
-	                 					})
-  }
-  }
-	
-	if(req.body.result.action=='hotel')
-  {
-	        var result=req.body.result;
-			var context=result.contexts[0];
-			var phonenumber=context.parameters.phonenumber;
-	  if(phonenumber.length<10)
-	  {
-   res.status(200).json({
-           				source: 'webhook',
-          					speech: 'Invalid phone number',
-           					displayText: 'Invalid phone number',
-		    				"followupEvent":{
-						"name":"re_phone",
-							"data":
-							{
-								
-								"phonenumber":""
-							}
-						}
-	                 					})
-	  }
-   else
-   {
-	  	 res.status(200).json({
-           					source: 'webhook',
-          					speech: 'Please share your email',
-           					displayText: 'Please share your email',
-		    				
-            					}) 
-   }
+	      /* "speech": "The value was invalid so I changed it to its default. What is your value for MyField2?",
+            "displayText": "The value was invalid so I changed it to its default. What is your value for MyField2?",
+            "contextOut": [
+                {
+                    //"name": "dc324995-cd5d-48b2-a74d-d2fca6fa0cc3_id_dialog_context",
+			"name":"HotelBooking",
+                    "parameters": {
+                         "check_in":"",
+			 "check_out":"",
+			  "adults":"3"
+                    },
+                    "lifespan": 2
+                }
+            ]*/
+            					})
   }
 if(req.body.result.action=='Dealerapi')
   {
